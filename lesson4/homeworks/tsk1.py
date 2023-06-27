ce65e1ab0c49145ada48029802ca9ac2903d6f24
ilias <ilias@prime.ru>
2023-06-27 11:10:52 +0500

    seminar 4 homeworks completed

--- lesson4/homeworks/tsk1.py
+++ lesson4/homeworks/tsk1.py
@@ -1,3 +1,18 @@
 """
 Напишите функцию для транспонирования матрицы
 """
+from random import randint
+
+
+def trans_matrix(matrix2d: list[list]):
+    return [[row[ind] for row in matrix2d] for ind in range(len(matrix2d[0]))]
+
+
+if __name__ == '__main__':
+    matrix = [[randint(1, 10) for _ in range(3)] for _ in range(2)]
+    print('Main matrix:')
+    for i in matrix:
+        print('\t'.join(map(str, i)))
+    print('\nTransformed matrix:')
+    for i in trans_matrix(matrix):
+        print('\t'.join(map(str, i)))
--- lesson4/homeworks/tsk2.py
+++ lesson4/homeworks/tsk2.py
@@ -4,3 +4,15 @@
 переданного аргумента, а значение — имя аргумента. Если
 ключ не хешируем, используйте его строковое представление.
 """
+
+
+def some_function(**kwargs):
+    some_dict = dict()
+    for some_key, some_value in kwargs.items():
+        some_dict[some_value] = some_key
+    return some_dict
+
+
+if __name__ == '__main__':
+    for key, value in some_function(var1=1, var2='ajdlf', var3=2.4, var4="alkjdf").items():
+        print(f'{key} => {value}')
