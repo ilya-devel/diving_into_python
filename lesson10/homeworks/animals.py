b58a45e492175702d9eb32f882b876e2677aec1b
Ilya.devel <ilya.devel@yandex.ru>
2023-07-18 16:47:03 +0500

    seminar 10 completed + added homeworks (some fix)

--- lesson10/homeworks/animals.py
+++ lesson10/homeworks/animals.py
@@ -8,8 +8,9 @@
 
 
 class Animal:
-    def __init__(self, name='Anonim'):
+    def __init__(self, name='Anonim', type_animal='unknown'):
         self.name = name
+        self.type_animal = type_animal
 
     def voice(self):
         pass
@@ -26,6 +27,9 @@ class Dog(Animal):
     def get_worker(self):
         return self.worker
 
+    def __str__(self):
+        return f"Эта {self.type_animal} по кличке {self.name}, работает {self.worker}"
+
 
 class Fish(Animal):
     def __init__(self, sea='Unknown', *args, **kwargs):
@@ -38,6 +42,9 @@ class Fish(Animal):
     def get_sea(self):
         return self.sea
 
+    def __str__(self):
+        return f"Эта {self.type_animal} по имени {self.name}, попала к нам из {self.sea}"
+
 
 class Cat(Animal):
     def __init__(self, family='Unknown', *args, **kwargs):
@@ -49,6 +56,9 @@ class Cat(Animal):
 
     def get_family(self):
         return self.family
+
+    def __str__(self):
+        return f"Эта {self.type_animal} по кличке {self.name}, живёт в семье {self.family}"
 
 
 if __name__ == '__main__':
--- lesson10/homeworks/fabric_animals.py
+++ lesson10/homeworks/fabric_animals.py
@@ -13,23 +13,25 @@ class FabricAnimals:
     def get_class(class_name: str, *args, **kwargs) -> object:
         if type(class_name) != str:
             raise ValueError("class_name должен быть строкой")
-
         raw_classes_ = Animal.__subclasses__()
         classes = {c.__name__: c for c in raw_classes_}
-        class_ = classes.get(class_name, None)
+        class__ = classes.get(class_name, None)
         if class_ is not None:
-            return class_(*args, **kwargs)
+            return class__(*args, **kwargs)
 
         raise Exception("Класс не найден")
 
 
 if __name__ == '__main__':
-    lst_animals = ['Cat', 'Dog', 'Horse']
+    lst_animals = [{'class': 'Cat', 'name': 'Гаф', 'type_animal': 'Сиамская', 'family': 'Бобровы'},
+                   {'class': 'Dog', 'name': 'Бобик', 'type_animal': 'Немецкая овчарка', 'worker': 'пограничник'},
+                   {'class': 'Horse', 'name': 'Звёздочка', 'type_animal': 'Владимирский тяжеловоз'}]
     for animal in lst_animals:
-        # show_animal = FabricAnimals().get_class(animal)
-        # print(show_animal.voice())
         try:
-            show_animal = FabricAnimals.get_class(animal)
-            print(f"Животное {animal} издаёт звуки: {show_animal.voice()}")
+            class_ = animal.pop('class')
+            show_animal = FabricAnimals.get_class(class_name=class_, **animal)
+            print(show_animal)
+            print(f"Животное {class_} издаёт звуки: {show_animal.voice()}")
+            print()
         except Exception as err:
-            print(f"Наша фабрика не занимается данной разновидностью животных ({animal})")
+            print(f"Наша фабрика не занимается данной разновидностью животных ({class_})")
